ba52edebd7662a04ae7098075fc64223da8490b5
pingole9960 <idipali593@gmail.com>
2025-04-23 23:25:20 +0530

    Create dialects.js

---
+++ src/dialects.js
@@ -0,0 +1,29 @@
+const dialects = [
+  {
+    standard: "How are you?",
+    solapuri: "Kasa aahes re tu?",
+    kolhapuri: "Kasa kay ba tu?"
+  },
+  {
+    standard: "My name is Raj",
+    solapuri: "Majha nav Raj ahe",
+    kolhapuri: "Majh nav Raj aahe re ba"
+  },
+  {
+    standard: "Where are you going?",
+    solapuri: "Kuthe chalalay re tu?",
+    kolhapuri: "Kuthe jatos ba tu?"
+  },
+  {
+    standard: "Let’s go",
+    solapuri: "Chal re jau",
+    kolhapuri: "Chal ba nighuya"
+  },
+  {
+    standard: "Thank you",
+    solapuri: "Baray re, dhanyavaad",
+    kolhapuri: "Dhanyavaad ho ba"
+  }
+];
+
+export default dialects;
